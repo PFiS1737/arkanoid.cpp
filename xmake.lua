3f8a12939e5299a2fc0a144a6d1a56cadff18200
PFiS1737 <pfis1737@gmail.com>
2025-06-24 10:46:35 +0800

    build: add xmake.lua

---
+++ xmake.lua
@@ -0,0 +1,26 @@
+add_rules("mode.debug", "mode.release")
+
+set_project("arkanoid.cpp")
+set_version("1.0.0")
+set_languages("c++20")
+
+add_requires("allegro")
+
+target("arkanoid.cpp", function()
+	set_kind("binary")
+
+	add_files("src/**.cpp")
+	add_headerfiles("src/**.hpp")
+
+	add_cxxflags("-Wall", "-Wextra", "-pedantic")
+
+	if is_mode("debug") then
+		add_cxxflags("-g", "-fsanitize=address,undefined")
+		add_ldflags("-fsanitize=address,undefined")
+		add_defines("DEBUG")
+	else
+		add_cxxflags("-O3")
+	end
+
+	add_packages("allegro")
+end)
